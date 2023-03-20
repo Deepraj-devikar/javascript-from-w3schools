ff12e5f654b64d16cc2a46b3126a9b01799faa36
unknown <deeprajdevikar19@gmail.com>
2023-03-20 11:58:14 +0530

    ADDED: fill

---
+++ array/methods/fill.js
@@ -0,0 +1,14 @@
+/**
+ * fill array with specific value
+ * 
+ * SYNTAX:- 
+ * array.fill(value, start, end)
+ */
+
+const array1 = [5, 2, 8, 9, 4];
+
+console.log('array1:- ', array1);
+
+array1.fill(0, 1, 3);
+
+console.log('array1:- ', array1);
